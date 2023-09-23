3bf3a6bb2271d201d62fbd4e0e9a8b32e082ae80
JewishLewish <lanbrokhman@gmail.com>
2023-09-23 09:07:21 -0700

    Convert images to tiff

--- src/main/java/com/jewishlewish/mis388/Mis388.java
+++ src/main/java/com/jewishlewish/mis388/Mis388.java
@@ -5,6 +5,14 @@
 package com.jewishlewish.mis388;
 
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FilenameUtils;
+
+import java.awt.image.BufferedImage;
+
 import net.sourceforge.tess4j.*;
 import net.sourceforge.tess4j.util.LoadLibs;
 
@@ -16,6 +24,7 @@ public class Mis388 {
 
     public static void main(String[] args) {
 
+        
         //Setup Tesseract OCR
         ITesseract instance = new Tesseract();  // JNA Interface Mapping
         File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build bundles English data
@@ -25,9 +34,20 @@ public class Mis388 {
         //instance.setTessVariable("load_system_dawg", "0");
         
 
-        //Use TIFF NOT PNG! PNG BREAKS EVERYTHING EVEN THE SCOPE OF THE UNNIVERSE!
-        File imageFile = new File("Resume-1.tiff");
+        
+        File imageFile;
 
+        //Use TIFF NOT PNG! PNG BREAKS EVERYTHING EVEN THE SCOPE OF THE UNNIVERSE!
+        
+        try {
+            if (!isTiffFormat("Resume-1.png")) {
+                convertToTiff("Resume-1.png", "Output.tiff");
+            } 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imageFile = new File("Resume-1.tiff");
         System.out.println(image_to_message(imageFile, instance));
     }
 
@@ -37,6 +57,22 @@ public class Mis388 {
             return result;
         } catch (TesseractException e) {
             return e.getMessage();
+        }
+    }
+
+    public static boolean isTiffFormat(String filePath) {
+        String fileExtension = FilenameUtils.getExtension(filePath).toLowerCase();
+        return fileExtension.equals("tiff") || fileExtension.equals("tif");
+    }
+
+    public static void convertToTiff(String inputFile, String outputFile) throws IOException {
+        BufferedImage image = ImageIO.read(new File(inputFile));
+
+        if (image != null) {
+            // Write the BufferedImage to the output TIFF file
+            ImageIO.write(image, "TIFF", new File(outputFile));
+        } else {
+            System.err.println("Failed to read input image: " + inputFile);
         }
     }
 }
