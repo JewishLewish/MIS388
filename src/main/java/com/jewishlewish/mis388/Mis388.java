847048059a490e03fe7a01814534b43505870935
JewishLewish <lanbrokhman@gmail.com>
2023-09-24 15:17:41 -0700

    Designed Custom Tess4j Wrapper

--- src/main/java/com/jewishlewish/mis388/Mis388.java
+++ src/main/java/com/jewishlewish/mis388/Mis388.java
@@ -4,17 +4,7 @@
 
 package com.jewishlewish.mis388;
 
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
-import org.apache.commons.io.FilenameUtils;
-
-import java.awt.image.BufferedImage;
-
-import net.sourceforge.tess4j.*;
-import net.sourceforge.tess4j.util.LoadLibs;
 
 /**
  *
@@ -22,57 +12,11 @@ import net.sourceforge.tess4j.util.LoadLibs;
  */
 public class Mis388 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        
-        //Setup Tesseract OCR
-        ITesseract instance = new Tesseract();  // JNA Interface Mapping
-        File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build bundles English data
-        instance.setDatapath(tessDataFolder.getPath());
-        instance.setTessVariable("user_defined_dpi", "1800");
-        instance.setTessVariable("tessedit_write_images", "true");
-        //instance.setTessVariable("load_system_dawg", "0");
-        
-
-        
-        File imageFile;
-
-        //Use TIFF NOT PNG! PNG BREAKS EVERYTHING EVEN THE SCOPE OF THE UNNIVERSE!
-        
-        try {
-            if (!isTiffFormat("Resume-1.png")) {
-                convertToTiff("Resume-1.png", "Output.tiff");
-            } 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        imageFile = new File("Resume-1.tiff");
-        System.out.println(image_to_message(imageFile, instance));
+        //Variables
+        String targetfile = "Resume.png";
+        System.out.println(tess4j.tesseract_read(targetfile));
     }
 
-    public static String image_to_message(File imageFile, ITesseract instance) {
-        try {
-            String result = instance.doOCR(imageFile);
-            return result;
-        } catch (TesseractException e) {
-            return e.getMessage();
-        }
-    }
-
-    public static boolean isTiffFormat(String filePath) {
-        String fileExtension = FilenameUtils.getExtension(filePath).toLowerCase();
-        return fileExtension.equals("tiff") || fileExtension.equals("tif");
-    }
-
-    public static void convertToTiff(String inputFile, String outputFile) throws IOException {
-        BufferedImage image = ImageIO.read(new File(inputFile));
-
-        if (image != null) {
-            // Write the BufferedImage to the output TIFF file
-            ImageIO.write(image, "TIFF", new File(outputFile));
-        } else {
-            System.err.println("Failed to read input image: " + inputFile);
-        }
-    }
 }
