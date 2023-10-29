ee89c8f995545335d609a7d32ee2d647f4db8011
JewishLewish <lanbrokhman@gmail.com>
2023-10-29 14:45:27 -0700

    Request Param to Map (KEY:VALUE)

--- src/main/java/mis/jewishlewish/demo/AppController.java
+++ src/main/java/mis/jewishlewish/demo/AppController.java
@@ -3,6 +3,7 @@ package mis.jewishlewish.demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AppController {
 
-    private List<String> items = new ArrayList<>();
+    //private List<String> items = new ArrayList<>(); - repurpose this later
 
     @GetMapping
     String getApp(Model model) {
@@ -28,11 +29,17 @@ public class AppController {
     }
 
     @PostMapping("/res")
-    public String processForm(@RequestParam("email") String item, Model model) {
-        items.add(item);
-        model.addAttribute("items", items);
+    public String processForm(@RequestParam Map<String, String> requestParams) {
+        
+        Py.print(requestParams.toString());
 
-        Py.print("Submitted item: " + item);
+        StringBuilder params = new StringBuilder("Request Parameters: \n");
+        for (Map.Entry<String, String> entry : requestParams.entrySet()) {
+            String Key = entry.getKey();
+            String Value = entry.getValue();
+            params.append(Key).append(": ").append(Value).append("\n");
+        }
+        System.out.println(params.toString());
 
         return "res";
     }
